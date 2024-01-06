04d946f1326f071bb50d464ff87daebde27cdd25
HelloWorld <dockyu.chen@gmail.com>
2024-01-06 14:16:06 +0800

    Update test_screen.dart

--- lib/screen/test_screen.dart
+++ lib/screen/test_screen.dart
@@ -23,15 +23,6 @@ class _TestScreenState extends State<TestScreen> {
     simpleClassInstance = await widget.rust.createSimpleClass(property: "Test Property");
   }
 
-  void _printProperty() async {
-    await widget.rust.printSimpleClassProperty(classInstance: simpleClassInstance);
-  }
-
-  void _getProperty() async {
-    String property = await widget.rust.getSimpleClassProperty(classInstance: simpleClassInstance);
-    print("Property from Rust: $property");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,20 +34,26 @@ class _TestScreenState extends State<TestScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: _printProperty,
+              onPressed: () async {
+                await widget.rust.printSimpleClassProperty(classInstance: simpleClassInstance);
+              },
               child: Text('Print Property'),
             ),
+            SizedBox(height: 20), // 增加間隔
             ElevatedButton(
-              onPressed: _getProperty,
+              onPressed: () async {
+                String property = await widget.rust.getSimpleClassProperty(classInstance: simpleClassInstance);
+                print("Property from Rust: $property");
+              },
               child: Text('Get Property'),
             ),
+            SizedBox(height: 20), // 增加間隔
             ElevatedButton(
               onPressed: () async {
                 await widget.rust.ffiIncrementAndPrintCount();
               },
               child: Text('count++ & print'),
             ),
-
           ],
         ),
       ),
